58d8bbda2271583d55fe3fbb913b571f4dda31e4
Markus <markus-musch@web.de>
2025-01-12 19:48:05 +0100

    add input, output window, send button and related callback

--- src/chatgmt/frontend/pages/chat_page.py
+++ src/chatgmt/frontend/pages/chat_page.py
@@ -1,9 +1,36 @@
+"""Chat page layout."""
 import dash  # type: ignore
 import dash_mantine_components as dmc  # type: ignore
 
-dash.register_page(__name__, path="/")
+from chatgmt.frontend.components.components_id_tree import ComponentIdTree
+from chatgmt.frontend.pages.callbacks_chat_page import *  # noqa: F401, F403
 
+dash.register_page(__name__)
 
 layout = dmc.Box(
-    dmc.Text("Here you can chat with large language models")
+    children=[
+        dmc.Card(
+            shadow="md",
+            h="500px",
+            children=[
+                dmc.Text(
+                    id=ComponentIdTree.App.ChatIds.OUTPUT,
+                    children=[
+                        "This is where the output will appear"
+                    ]
+                )
+            ]
+        ),
+        dmc.Textarea(
+            id=ComponentIdTree.App.ChatIds.INPUT,
+            label="Input",
+            value="",
+            placeholder="Type your message here",
+            m="md"
+        ),
+        dmc.Button(
+            id=ComponentIdTree.App.ChatIds.SEND_BUTTON,
+            children=["Send"],
+        )
+    ]
 )
