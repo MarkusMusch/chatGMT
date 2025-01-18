295e6cf08b329a630603d8e5d0ca68f5fd42c07e
Markus <markus-musch@web.de>
2025-01-18 10:45:04 +0100

    test git hook

---
+++ src/chatgmt/services/llm_service.py
@@ -0,0 +1,13 @@
+"""This module contains the service for the LLM chat."""
+
+
+def send_message(input: str) -> str:
+    """Send a message to the chat.
+
+    Args:
+        input: The message to send to the LLM.
+
+    Returns:
+        The message to send to the LLM.
+    """
+    return input
